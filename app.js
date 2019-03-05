57f699b2e17f3fd5a5e8e9ae078a52320e1d8d47
adambrgmn <adam@wordpressutvecklare.se>
2019-03-05 16:27:33 +0100

    Adding base structure for express and handlebars

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('Hello world!')
+const express = require('express')
+const path = require('path')
+const hbs = require('express-hbs')
+
+const app = express()
+const port = 3000
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}/`))
+
+// view engine setup
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views', 'layouts', 'default'),
+  partialsDir: path.join(__dirname, 'views', 'partials')
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Routes
+app.use('/', require('./routes/homeRouter'))
+
+// additional middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// catch 404
+app.use((req, res, next) => {
+  res.status(404)
+  res.sendFile(path.join(__dirname, 'public', '404.html'))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  if (err.message === '403') {
+    res.status(err.status || '403')
+    res.sendFile(path.join(__dirname, 'public', '403.html'))
+  } else if (err.status === '500') {
+    res.status(err.status || 500)
+    res.send(err.message || 'Internal Server Error')
+  }
+})
