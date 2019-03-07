6bec311c7c9d2fa058c4581270d058c9ec118cb8
adambrgmn <adam@wordpressutvecklare.se>
2019-03-07 16:10:44 +0100

    Debugging

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -13,24 +13,25 @@ const client = github.client(process.env.ACCESS_TOKEN)
 const ghrepo = client.repo('1dv023/ab224qr-examination-3')
 
 homeController.index = async (req, res, next) => {
-  try {
-    let result = await ghrepo.issuesAsync()
+  // try {
+  //   let result = await ghrepo.issuesAsync()
 
-    result = result[0].map(issue => ({
-      id: issue.number,
-      issue: issue.title,
-      description: issue.body,
-      comments: issue.comments,
-      state: issue.state,
-      created: issue.created_at.substr(0, 10),
-      time: issue.created_at.substr(11, 5),
-      url: issue.html_url
-    }))
+  //   result = result[0].map(issue => ({
+  //     id: issue.number,
+  //     issue: issue.title,
+  //     description: issue.body,
+  //     comments: issue.comments,
+  //     state: issue.state,
+  //     created: issue.created_at.substr(0, 10),
+  //     time: issue.created_at.substr(11, 5),
+  //     url: issue.html_url
+  //   }))
 
-    res.render('home/index', { issues: result })
-  } catch (err) {
-    console.log(err)
-  }
+  //   res.render('home/index', { issues: result })
+  // } catch (err) {
+  //   console.log(err)
+  // }
+  res.render('home/index')
 }
 
 // Exports.
