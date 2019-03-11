6a8b46bf3cb52a8a8e50251e3fa023bdf3633aec
adambrgmn <adam@wordpressutvecklare.se>
2019-03-11 13:37:42 +0100

    Adding ability to remove an issue

--- public/js/client.js
+++ public/js/client.js
@@ -1,17 +1,21 @@
 const socket = window.io.connect('https://d74e410a.ngrok.io')
 
-console.log('Hello from js folder')
+console.log('Hello from public JS folder')
 
-socket.on('changeCommentCount', issue => {
-  console.log('Adding updates to a comment')
+socket.on('changeComments', issue => {
+  // Updating comment count for the issue
   const currentIssueComments = document.querySelector(`#issue-${issue.id} .issue-comments`)
   currentIssueComments.textContent = issue.comments + 1
 })
 
-socket.on('newIssue', issue => {
-  console.log('Receiving new issue')
-  console.log(issue.title)
+socket.on('closed', issue => {
+  // Removing the issue
+  const currentIssue = document.querySelector(`#issue-${issue.id}`)
+  currentIssue.parentNode.removeChild(currentIssue)
+})
 
+socket.on('newIssue', issue => {
+  // Creating a new issue
   let mainDiv = document.querySelector('.list-of-issues')
   const issueClone = document.querySelector('.issue')
   let template = document.importNode(issueClone, true)
--- routes/webhook.js
+++ routes/webhook.js
@@ -8,38 +8,36 @@
 const express = require('express')
 const router = express.Router()
 
-router
-// .get('/webhook', (req, res, next) => {
-//   console.log(res)
-//   res.sendStatus(200)
-// })
-  .post('/', async (req, res) => {
-    const io = req.app.get('socketio')
+router.post('/', async (req, res) => {
+  const io = req.app.get('socketio')
 
-    let body = JSON.parse(req.body)
-    // console.log(body)
+  let body = JSON.parse(req.body)
 
-    const issue = {
-      id: body.issue.id.toString(),
-      number: body.issue.number,
-      title: body.issue.title,
-      description: body.issue.body,
-      comments: body.issue.comments,
-      state: body.issue.state,
-      created: body.issue.created_at.substr(0, 10),
-      time: body.issue.created_at.substr(11, 5),
-      url: body.issue.html_url
-    }
-    if (body.action === 'created') {
-      io.emit('changeCommentCount', {
-        id: issue.id, action: body.action, comments: issue.comments
-      })
-    }
+  const issue = {
+    id: body.issue.id.toString(),
+    number: body.issue.number,
+    title: body.issue.title,
+    description: body.issue.body,
+    comments: body.issue.comments,
+    state: body.issue.state,
+    created: body.issue.created_at.substr(0, 10),
+    time: body.issue.created_at.substr(11, 5),
+    url: body.issue.html_url
+  }
+  if (body.action === 'created') {
+    io.emit('changeComments', {
+      id: issue.id,
+      comments: issue.comments
+    })
+  }
+  if (body.action === 'opened') {
+    io.emit('newIssue', issue)
+  }
+  if (body.action === 'closed') {
+    io.emit('closed', { id: issue.id })
+  }
 
-    if (body.action === 'opened') {
-      io.emit('newIssue', issue)
-    }
-    res.sendStatus(200)
-  })
+  res.sendStatus(200)
+})
 
 module.exports = router
